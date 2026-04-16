dd47b8c1795c00611723fdf31c9605f6505367a0
Abdallah Fekry <abdallahfekry95@gmail.com>
2026-04-16 07:00:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
-st.logo("media/logo.png")
-
 about = """Your about info"""
 menu_items = {
 "Get help": "mailto:@abdallahfekry95@gmail.com",
 "About": about}
 st.set_page_config(page_title="Findora", page_icon='media/icon.png', initial_sidebar_state='collapsed', layout='centered', menu_items=menu_items)
+
+st.logo("media/logo.png")
 
 if "first_time" not in st.session_state:
   st.session_state.first_time = True
@@ -22,9 +22,6 @@ if "chat_history" not in st.session_state:
 
 home = st.Page("home.py", title="Home", icon=":material/home:", default=True)
 chat = st.Page("chat.py", title="Chatbot", icon=":material/smart_toy:")
-# test = st.Page("test.py", title="Test", icon=":material/settings:")
-# testbot = st.Page("testbot.py", title="Test Bot", icon=":material/smart_toy:")
-# manual = st.Page("manual_search.py", title="Manual Search", icon=":material/search:")
 pg = st.navigation([home, chat])
 
 # if st.session_state.logged_in:   
